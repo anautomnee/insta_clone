a7db03368cd6a61f6348d7fd4a549ce5dbe883a6
Anastasiia Kozlova <anautomnee@gmail.com>
2024-12-18 15:03:51 +0100

    Remove unused imports

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,15 +1,11 @@
 import express from 'express';
-import User from '../db/models/User.js';
 import 'dotenv/config';
-import nodemailer from "nodemailer";
 import {loginUser, registerUser, resetPassword} from "../controllers/authController.js";
 
 const router = express.Router();
 
 router.post('/login', loginUser);
-
 router.post("/register", registerUser);
-
 router.post("/reset", resetPassword);
 
 export default router;
