d558c82b3d8802f275fe86293305e78ead17d901
Anastasiia Kozlova <anautomnee@gmail.com>
2024-12-24 15:34:26 +0100

    Apply image position in post

--- client/src/components/PostModal/PostModal.tsx
+++ client/src/components/PostModal/PostModal.tsx
@@ -19,20 +19,22 @@ export const PostModal = ({post, currentPostRef, setCurrentPost}: PostModalProps
 
     return (<div
         className="fixed h-[calc(100vh-81px)] md:min-h-screen top-0 w-screen
-            lg:w-[calc(100vw-60px)] left-0 md:left-[60px] lgg:left-[244px]"
+            lg:w-[calc(100vw-60px)] lgg:w-[calc(100vw-244px)] left-0 md:left-[60px] lgg:left-[244px]"
         style={{backgroundColor: 'rgba(0, 0, 0, 0.65)'}}
         onClick={closePostModal}
     >
         <div className="bg-white opacity-100 mt-8 md:mt-36 mx-auto rounded
-            xl:w-[913px] lg:w-[720px] md:w-[510px] w-[90vw]"
+            xl:w-[913px] lg:w-[720px] lgg:w-[1000px] md:w-[510px] w-[90vw]"
              onClick={(e: MouseEvent<HTMLDivElement>) => e.stopPropagation()}>
             <div className="flex">
-                <div className="w-[577px]">
+                <div className="flex justify-center items-center
+                 border-r border-gray min-w-[577px] min-h-[577px]">
                     <img
                     src={post?.photo}
-                    alt="Post"/>
+                    alt="Post"
+                    className="w-full h-full object-contain"/>
                 </div>
-                <div>
+                <div className="w-full">
                     <div className="border-b border-b-gray">
                         <p className="mx-3.5 my-4 text-xs">{post?.author?.username}</p>
                     </div>
