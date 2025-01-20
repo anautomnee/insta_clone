78f348d36bd5d334825df0344298474c8a605cf7
Anastasiia Kozlova <anautomnee@gmail.com>
2025-01-20 20:33:46 +0100

    Add build to front

--- client/src/routes/PrivateRoute.tsx
+++ client/src/routes/PrivateRoute.tsx
@@ -4,14 +4,26 @@ import {checkJWTToken} from "../uitls/apiCalls.ts";
 
 export const PrivateRoute = ({ children }: { children: ReactNode }) => {
     const [isNotAuthenticated, setIsNotAuthenticated] = useState<boolean | null>(null);
+    const [isLoading, setIsLoading] = useState(true); // Состояние загрузки токена
     const location = useLocation();
+
     useEffect(() => {
         const handleCheck = async () => {
             const response = await checkJWTToken();
             setIsNotAuthenticated(!response);
-        }
+            setIsLoading(false); // После завершения проверки устанавливаем состояние загрузки в false
+        };
+
         handleCheck();
-    }, [[location.pathname]]);
-    if (isNotAuthenticated === null) return;
-    return isNotAuthenticated ? <Navigate to='/login' replace/> : children;
+    }, [location.pathname]);
+
+    if (isLoading) {
+        return <div>Loading...</div>; // Пока идет проверка токена, можно показать загрузочный экран
+    }
+
+    return isNotAuthenticated ? (
+        <Navigate to="/Login" replace />
+    ) : (
+        children
+    );
 };
