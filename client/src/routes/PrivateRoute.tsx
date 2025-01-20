d5b962bb6a389f1b1f4c052131745cefd6db86c2
Anastasiia Kozlova <anautomnee@gmail.com>
2025-01-20 21:16:09 +0100

    Add isLoading to PrivateRoute

--- client/src/routes/PrivateRoute.tsx
+++ client/src/routes/PrivateRoute.tsx
@@ -4,14 +4,24 @@ import {checkJWTToken} from "../uitls/apiCalls.ts";
 
 export const PrivateRoute = ({ children }: { children: ReactNode }) => {
     const [isNotAuthenticated, setIsNotAuthenticated] = useState<boolean | null>(null);
+    const [isLoading, setIsLoading] = useState<boolean>(true);
     const location = useLocation();
     useEffect(() => {
         const handleCheck = async () => {
             const response = await checkJWTToken();
             setIsNotAuthenticated(!response);
+            setIsLoading(false);
         }
         handleCheck();
     }, [[location.pathname]]);
     if (isNotAuthenticated === null) return;
-    return isNotAuthenticated ? <Navigate to='/login' replace/> : children;
+    if (isLoading) {
+        return <div>Loading...</div>; // Пока идет проверка токена, можно показать загрузочный экран
+    }
+
+    return isNotAuthenticated ? (
+        <Navigate to="/Login" replace />
+    ) : (
+        children
+    );
 };
