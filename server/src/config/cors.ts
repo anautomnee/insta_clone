95a940f1c08d192fec879d07d1509c5009966771
Anastasiia Kozlova <anautomnee@gmail.com>
2025-01-22 23:25:15 +0100

    update cors config

--- server/src/config/cors.ts
+++ server/src/config/cors.ts
@@ -5,6 +5,10 @@ export const configureCors = (): CorsOptions => {
     const clientHost = process.env.ENV === "local" ? "http://localhost" : process.env.CLIENT_HOST;
     return {
         origin: `${clientHost}`,
+        methods: ["GET", "POST", "DELETE", "PUT"],
+        allowedHeaders: ["Content-Type", "Authorization"],
+        exposedHeaders: ["Set-Cookie"],
+        preflightContinue: false,
         credentials: true,
     };
 };
